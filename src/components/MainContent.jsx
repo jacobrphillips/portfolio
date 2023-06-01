d04c0a2e4141506eb0bac1051e2ff0bdd5a26272
Jacob Phillips <0123phillips@gmail.com>
2023-06-01 23:00:50 +0100

    creates the overall react component structure

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './header/Header';
+import Footer from './footer/Footer';
+import Body from './body/Body';
+import styles from './MainContent.module.css';
+
+const MainContent = () => {
+  return (
+    <>
+    <Header />
+    <Body />
+    <Footer />
+    </>
+  )
+}
+
+export default MainContent
---
+++ src/components/body/Body.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Body.module.css';
+
+const Body = () => {
+  return (
+    <div>Body</div>
+  )
+}
+
+export default Body
